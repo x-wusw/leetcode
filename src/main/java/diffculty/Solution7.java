a38bf59dbbc74cf0680fb95cae08c977ce2eef6a
shenwu <963527544@qq.com>
2020-04-24 19:31:16 +0800

    求逆序对

--- src/main/java/diffculty/Solution7.java
+++ src/main/java/diffculty/Solution7.java
@@ -10,7 +10,7 @@ package diffculty;
 public class Solution7 {
     public static void main(String[] args) {
         Solution7 s = new Solution7();
-        int[] nums = {5,4,2,1};
+        int[] nums = {7,5,6,4};
         System.out.println(s.reversePairs(nums));
     }
     private int reversePairs(int[] nums) {
@@ -41,7 +41,7 @@ public class Solution7 {
         int leftPairs = mergesort(nums,left,mid,temp);
         int rightPairs = mergesort(nums,mid+1,right,temp);
         if(nums[mid] <= nums[mid+1]){   //左边数组的最大值<=右边数组的最大值则直接返回左边逆序对加上右边的逆序对
-            return leftPairs+rightPairs;
+            return leftPairs + rightPairs;
         }
         int curPairs = mergeAndCont(nums,left,mid,right,temp);
         return leftPairs+rightPairs+curPairs;
@@ -66,19 +66,19 @@ public class Solution7 {
         int count = 0;
         for (int k = left; k <= right ; k++) {
             if(i == mid + 1){       //左边已归并完
-                temp[k] = nums[j];
+                nums[k] = temp[j];
             }
             else if(j == right + 1){//右边已归并完
-                temp[k] = nums[i];
+                nums[k] = temp[i];
             }
-            else if(nums[i] <= nums[j]){//左边小于右边，把左边放入
-                temp[k] = nums[i];
+            else if(temp[i] <= temp[j]){//左边小于右边，把左边放入
+                nums[k] = temp[i];
                 i++;
             }
             else {
-                temp[k] = nums[j];//右边小于左边，把右边放入
+                nums[k] = temp[j];//右边小于左边，把右边放入
                 j++;
-                count += mid-i+1; //逆序对数即为左边数组还剩下的数的个数
+                count += (mid-i+1); //逆序对数即为左边数组还剩下的数的个数
             }
         }
         return count;
